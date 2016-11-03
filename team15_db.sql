35073f150266d4a84f12c6866de34a5668ee6f2b
machadoh <hhm4@pitt.edu>
2016-11-03 16:34:03 -0400

    Create team15_db.sql

---
+++ team15_db.sql
@@ -0,0 +1,37 @@
+//Database schema PittTours
+
+drop table AIRLINE cascade constraints;
+drop table FLIGHT cascade constraints;
+drop table PLANE cascade constraints;
+drop table PRICE cascade constraints;
+drop table CUSTOMER cascade constraints;
+drop table RESERVATION cascade constraints;
+drop table TIME cascade constraints;
+
+create table AIRLINE(
+
+);
+
+create table FLIGHT(
+
+);
+
+create table PLANE(
+
+);
+
+create table PRICE(
+
+);
+
+create table CUSTOMER(
+
+);
+
+create table RESERVATION(
+
+);
+
+create table TIME(
+
+);
