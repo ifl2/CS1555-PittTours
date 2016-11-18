b554c1c27b0ac61e0b4cb1b7d92bf9ee15a303d0
Ian Leonard <ifl2@pitt.edu>
2016-11-17 19:59:18 -0500

    Some fixes

--- team15-triggers.sql
+++ team15-triggers.sql
@@ -8,85 +8,90 @@ You should create a trigger, called adjustTicket, that adjusts the cost of a res
 3. find if it is ticked or not (join 2 with RESERVATIONS using reservation_number)
 4. if ticketed = 'Y' do not do anything
 5. if ticketed = 'N' update R.cost( find all legs from each reservation, add the D.cost(each leg) and update the final cost( R.cost)*/
-				
-create trigger adjustTicket				 
+
+-- BROKEN
+create or replace trigger adjustTicket
 after update of low_price
 on PRICE
 for each row
-
-select r.reservation_number, SUM(r.cost) as new_cost
+select r.reservation_number, SUM(reservation.cost) as new_cost
 from (((FLIGHT f1 full join PRICE p1 on p1.departure_city=f1.departure_city and p1.arrival_city=f1.arrival_city) full join DETAIL d1 on f1.flight_number = d1.flight_number) full join reservation r on d1.reservation_number=r.reservation_number)
 where d1.reservation_number IN (select d.reservation_number
 from (price p full join flight f on p.departure_city=f.departure_city and p.arrival_city=f.arrival_city) full join detail d on f.flight_number = d.flight_number
 where p.departure_city=:new.departure_city and p.arrival_city=:new.arrival_city)
 group by r.reservation_number;
 
-when( EXISTS( 	SELECT  cid, ticketed
+
+when( EXISTS(	SELECT	cid, ticketed
 		FROM RESERVATION
-		WHERE = 'N')
+		WHERE ticketed = 'N'))
 BEGIN
+
 UPDATE RESERVATION R4
 SET R4.cost = new_cost
 where R4.reservation_number = r.reservation_number AND 'N' = (select ticketed from RESERVATION R1
 where R1.reservation_num = r.reservation_number)
 END;
 /
-show errors;
-       
+
 /* Trigger 2 */
 
-create or replace procedure count_flight(in number varchar(3), out a_count integer)
-AS
-BEGIN
-select count(flight_number) into a_count
-from DETAILS
-where DETAILS.flight_number =number;
-end;
-/
-show errors;
-
-create or replace function is_full(IN number varchar(3), IN capacity int)
-returns boolean
+create or replace procedure count_flight(f_number in varchar, a_count out int)
+as
 begin
-select plane_type, flight_capacity
-From(FIGHT F INNER JOIN PLANE P on F.plane_type = P.plane_type)
-where flight_number = number
-return flight_capacity = capacity;
+	select count(flight_number) into a_count
+	from detail
+	where flight_number = f_number;
 end;
 /
-show errors;
+
+create or replace function is_full(f_number in varchar, capacity in int)
+return boolean
+as
+p_cap int;
+begin
+	select plane.plane_capacity into p_cap
+	from flight, plane
+	where flight_number = f_number and flight.plane_type = plane.plane_type;
+	return (p_cap = capacity);
+end;
+/
 
 create or replace trigger planeUpgrade
-before update of reservation_number
-on RESERVATION
+before update
+on detail
 for each row
+declare
+	flight_n varchar(3);
+	flight_c int;
+	max_capacity int;
 begin
-select flight_number
-from DETAIL D
-where D.flight_number= :new.flight_number
+	select flight_number into flight_n
+	from detail
+	where flight_number = :new.flight_number;
 
+	count_flight(flight_n, flight_c);
 
-declare count =  count_flight(D.flight_number)
-when(EXISTS( is_full(flight_number,count)))
-select MAX(plane_capacity) into max_capacity
-from plane
-if max_capacity = count then
-return;
-else
-UPDATE flight
-set plane_type = (select plane_type
-		   from PLANE
-		   where flight_capacity > count AND owner_id =(select airline_id from FLIGHT F where F.flight_number = (select D1.flight_number from DETAIL D1 where D1.reservation_number = :new.reservation_number)
-	   	   order by flight_capacity DESC
-		   fetch first row only;))
-where flight_number=D.flight_number;
-END IF;
+	if is_full(flight_n, flight_c) then
+		select max(plane_capacity) into max_capacity
+		from plane;
+		if max_capacity != flight_c then
+			update flight
+			set plane_type = (
+				select plane_type
+				from plane
+				where plane_capacity > flight_c and owner_id = (select airline_id from flight where flight.flight_number = (select detail.flight_number from detail where reservation_number = :new.reservation_number and flight_number = :new.flight_number)
+				order by plane_capacity desc
+				fetch first row only))
+			where flight_n = :new.flight_number;
+		end if;
+	end if;
 end;
 /
-show errors;
-/* Trigger 3 
-You should write a trigger, called cancelReservation, that cancels(deletes)all non-ticketed reservations for a flight, 12 hours prior 
-the flight (i.e., 12 hours before the flight is scheduled to depart) and if the number of ticketed passengers fits in a smaller capacity 
+
+/* Trigger 3
+You should write a trigger, called cancelReservation, that cancels(deletes)all non-ticketed reservations for a flight, 12 hours prior
+the flight (i.e., 12 hours before the flight is scheduled to depart) and if the number of ticketed passengers fits in a smaller capacity
 plane, then the plane for that flight should be switched to the smaller-capacity plane
 
 1. find all flights that are 12h before depart in FLIGHT table(check week date and time)
@@ -96,45 +101,54 @@ plane, then the plane for that flight should be switched to the smaller-capacity
 5. if ticketed = 'Y' count all the tickets for each flight
 6. use the count to find a smaller plane if possible( select plane_type from PLANE where plane_capacity >= count , sort by min first)
 7. if find a smaller plane, update FLIGHT(where F.plane_type = P.plane_type) */
-  
-create or replace trigger CancelReservation				 
-after update of c_date
-on TIME
-for each row
-     
-DELETE FROM RESERVATION
-where reservation_number IN (select R.reservation_number
-group by R.reservation_number
-from RESERVATION R
-where ticketed = 'N' AND R.reservation_number IN (select D.reservation_number
-from FLIGHT natural join DETAIL
-where D.flight_number IN (select flight_number 
-from FLIGHT natural join RESERVATION
-where (reservation_date <= dateadd(HOUR, 12, c_date)))))
 
-declare count = count_flight(D.flight_number)
-UPDATE FLIGHT
-set plane_type = (select plane_type
-from PLANE
-where plane_capacity >= count AND owner_id =(select airline_id from FLIGHT F where F.flight_number = (select D1.flight_number from DETAIL D1 where D1.flight_number = D.flight_number)
-order by flight_capacity DESC
-fetch first row only;))
+-- BROKEN
+create or replace trigger CancelReservation
+after update
+on Our_Date
+for each row
+declare
+	flight_c int;
+begin
+	delete from reservation
+	where reservation_number in (
+		select reservation_number
+		from reservation R
+		group by reservation_number
+		where ticketed = 'N' and reservation_number in (
+			select reservation_number
+			from flight natural join detail
+			where flight_number in (
+				select flight_number
+				from flight natural join reservation
+				where (reservation_date <= dateadd(HOUR, 12, c_date))
+			)
+		)
+	)
+
+	count_flight(flight_n, flight_c);
+
+	update flight
+	set plane_type =
+		(select plane_type
+		from PLANE
+		where plane_capacity >= flight_c and owner_id = (select airline_id from flight where flight.flight_number = (select detail.flight_number from detail where reservation_number = :new.reservation_number and flight_number = :new.flight_number)
+		order by plane_capacity desc
+		fetch first row only));
 end;
 /
-show errors;					  
-						  
-						  
+
+
 /*Trigger extra*/
 create or replace trigger ten_percent
-after insert of reservation_number
-on RESERVATION
+after insert
+on reservation
 for each row
-
+begin
 update reservation
-set cost=cost*0.9
-where cid = (select cid from customer
-where frequent_miles != null)
-
+set cost = (cost * 0.9)
+where cid =
+	(select cid from customer
+	where frequent_miles != null);
 end;
 /
-show errors;
